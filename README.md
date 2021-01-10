95c2942cd06ec31f81a3ad73c4476d8f9ca72c19
frontendercode <frontendercode@gmail.com>
2021-01-10 17:02:02 +0200

    v.0.45
    Выполнено контентное наполнение шаблона

--- README.md
+++ README.md
@@ -1,20 +1,21 @@
-# essentials
+# Шаблон essentials
 
-Верстаю HTML-5 шаблон, макет для него взял из интернета с внесением визуальных изменений в макет. Шаблон получился минималистичным. На его основе будет создан шаблон для многостраничного сайта.
-К блоками шаблона была привязана легкая анимация.
-
+Верстаю HTML-5 шаблон для своего портфолио. Макет в формате PSD взял в интернете. После начального этапа верстки пришлось внести много изменений. Затем полностью переписал текст. 
+Шаблон выполнен в минималистичном стиле, для разнообразия и демонстрации своих навыков настроил простую анимацию блоков. Для усложнения работа на снове одностраничного макета создал шаблон для многостраничного сайта. Конечно же дизайн внутрених страниц был выполнен на свой страх и риск, так как я на сегодняшний день не выполняю разработку дизайна для интернет-макетов.
 # Адаптивность
 
-Шаблон будет адаптироваться исходя требований сетки bootstrap 4
+Адаптивность шаблона была выполнена, исходя из требований сетки bootstrap 4. На практике выполняю адаптацию ориентируясь на требования заказчика.
 
 ## Связаться по вопросам создания сайта под ключ:
 
-- [facebook](https://www.facebook.com/frontendercode)
-- [github](https://github.com/frontend-coder)
-- [skype:webrabcom](href="skype:webrabcom")
-- [telegram](https://t.me/frontendcoder)
+[![facebook](https://img.shields.io/badge/-Facebook-1877F2?style=for-the-badge&logo=Figma&logoColor=eeffff)](https://www.facebook.com/frontendercode)
+[![Telegram](https://img.shields.io/badge/-Telegram-26A5E4?style=for-the-badge&logo=Telegram&logoColor=eeffff)](https://t.me/frontendcoder)
+[![Instagram](https://img.shields.io/badge/-Instagram-E4405F?style=for-the-badge&logo=Instagram&logoColor=eeffff)](https://www.instagram.com/frontendercode/?hl=ru)
+[![GitHub](https://img.shields.io/badge/-GitHub-181717?style=for-the-badge&logo=GitHub&logoColor=eeffff)](https://github.com/frontend-coder)
 
-## Портфолио
 
-- [Портфолио](https://frontend-coder.github.io)
+
+[![Portfolio](https://img.shields.io/badge/-Портфолио-181717?style=for-the-badge&logo=Internet-Archive&logoColor=eeffff)](https://frontend-coder.github.io)
+
+
 
--- app/libs/common.js
+++ app/libs/common.js
@@ -1,17 +1,15 @@
-$(document).ready(function () {
-
-
+$(document).ready(() => {
 // // variables
 // var $header_top = $('.header-top');
 // var $nav = $('nav');
 
-// // toggle menu
-// $header_top.find('a').on('click', function() {
-//   $(this).parent().toggleClass('open-menu');
-// });
+  // // toggle menu
+  // $header_top.find('a').on('click', function() {
+  //   $(this).parent().toggleClass('open-menu');
+  // });
 
-  $(function() {
-    $('.acc-btn').click(function() {
+  $(() => {
+    $('.acc-btn').click(function () {
       $('.acc-content').slideUp('normal');
       $('.acc-btn').removeClass('acc-active');
       if ($(this).next().is(':visible') == true) {
@@ -28,53 +26,52 @@ $(document).ready(function () {
     $('.show').show();
   });
 
-// let priceBlock = $('.price_block');
-// let topBlock =  priceBlock.offset().top;
-// $(window).bind('scroll', function() {
-// let windowTop = $(this).scrollTop();
-// if(windowTop > topBlock ) {
-//   $('#map_wrap').html('<div id="map" style="width: 100%; height: 470px"></div>');
-// $(window).unbind('scroll');
-// }
-// });
+  // let priceBlock = $('.price_block');
+  // let topBlock =  priceBlock.offset().top;
+  // $(window).bind('scroll', function() {
+  // let windowTop = $(this).scrollTop();
+  // if(windowTop > topBlock ) {
+  //   $('#map_wrap').html('<div id="map" style="width: 100%; height: 470px"></div>');
+  // $(window).unbind('scroll');
+  // }
+  // });
 
- $(".owl-carousel").owlCarousel({
-loop                : true,
-margin              : 50,
-stagePadding        : 50,
-autoplay            : true,
-smartSpeed          : 1000,
-//  autoplayTimeout : 1000,
-autoplayHoverPause  : true,
-items: 4,
-    responsiveClass:true,
-    responsive:{
-        0:{
-            items:1
-        },
-600:{
-            items:2
-        },
-        800:{
-            items:3
-        },
-        1080:{
-            items:4
-        }
-    }
- });
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 50,
+    stagePadding: 50,
+    autoplay: true,
+    smartSpeed: 1000,
+    //  autoplayTimeout : 1000,
+    autoplayHoverPause: true,
+    items: 4,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 2,
+      },
+      800: {
+        items: 3,
+      },
+      1080: {
+        items: 4,
+      },
+    },
+  });
 
-
-
-  //Ajax push mesege http://api.jquery.com/jquery.ajax/
-  $("form").submit(function() { //Change
-    var th = $(this);
-    $.ajax({type: "POST",
-      url: "mail.php", //Change
-      data: th.serialize()
-    }).done(function () {
+  // Ajax push mesege http://api.jquery.com/jquery.ajax/
+  $("form").submit(function () { // Change
+    const th = $(this);
+    $.ajax({
+      type: "POST",
+      url: "mail.php", // Change
+      data: th.serialize(),
+    }).done(() => {
       $(".form_footer .success").addClass("active");
-      setTimeout(function () {
+      setTimeout(() => {
         // Done Functions
         $(".form_footer .success").removeClass("active");
         th.trigger("reset");
@@ -83,31 +80,27 @@ items: 4,
     });
     return false;
   });
-  //castom code
+  // castom code
 
-
-AOS.init({
+  AOS.init({
   // Global settings:
- 
- 
-  startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
-  initClassName: 'aos-init', // class applied after initialization
-  animatedClassName: 'aos-animate', // class applied on animation
-  useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
-  disableMutationObserver: false, // disables automatic mutations' detections (advanced)
-  debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
-  throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
-  disable: 'mobile', // accepts following values: 'phone', 'tablet', 'mobile',
 
-  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-  offset: 100, // offset (in px) from the original trigger point
- // delay: 1000, // values from 0 to 3000, with step 50ms
-  duration: 800, // values from 0 to 3000, with step 50ms
-  easing: 'ease', // default easing for AOS animations
- // whether animation should happen only once - while scrolling down
-  mirror: false, // whether elements should animate out while scrolling past them
-  anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
-});
+    startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+    initClassName: 'aos-init', // class applied after initialization
+    animatedClassName: 'aos-animate', // class applied on animation
+    useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+    disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+    debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+    throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+    disable: 'mobile', // accepts following values: 'phone', 'tablet', 'mobile',
 
-
+    // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+    offset: 100, // offset (in px) from the original trigger point
+    // delay: 1000, // values from 0 to 3000, with step 50ms
+    duration: 800, // values from 0 to 3000, with step 50ms
+    easing: 'ease', // default easing for AOS animations
+    // whether animation should happen only once - while scrolling down
+    mirror: false, // whether elements should animate out while scrolling past them
+    anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+  });
 });
